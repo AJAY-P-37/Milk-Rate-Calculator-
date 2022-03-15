dbde1b353b832178a9e666ca5e9c54d4423e2064
AJAY-P-37 <ajaykri3.7@gmail.com>
2022-03-15 21:58:10 +0530

    initial

---
+++ index.js
@@ -0,0 +1,39 @@
+let arr = [22.5, 11, 21.5, 9.5, 23.5, 10, 52, 24.5]
+
+for (let i = 0; i < 8; i++) {
+    let milkType = document.querySelector(`#milkTable > table > tbody > tr:nth-child(${i + 1}) > th:nth-child(3) > input`)
+    milkType.addEventListener('input', (e) => {
+        let value = parseFloat(milkType.value)
+        console.log(value)
+
+        let milk_costs = document.querySelector(`#milkTable > table > tbody > tr:nth-child(${i + 1}) > th:nth-child(4)`)
+        if (!isNaN(value)) {
+            milk_costs.innerHTML = value * arr[i]
+
+        } else {
+            milk_costs.innerHTML = 0
+        }
+
+        let total = 0
+
+        for (let i = 0; i < 8; i++) {
+
+            let milk = document.querySelector(`#milkTable > table > tbody > tr:nth-child(${i + 1}) > th:nth-child(4)`)
+            console.log(milk.innerHTML)
+            if (milk.innerHTML != '') {
+                let t_value = parseFloat(milk.innerHTML)
+                total += t_value
+            }
+
+
+        }
+
+        total_value = document.querySelector("#milkTable > table > tfoot > tr > th.total_value")
+        total_value.innerHTML = total
+
+    })
+}
+
+
+
+
