3b0c4acb3c41b863ad34ec3864a01f8f9fcb72e6
AJAY-P-37 <ajaykri3.7@gmail.com>
2023-11-11 12:53:48 +0530

    Data from firebas

--- index.js
+++ index.js
@@ -1,46 +1,85 @@
-let arr = [];
-
-let val = document.querySelectorAll(`tbody > tr > th:nth-child(2)`);
-val.forEach((val) => {
-  arr.push(+val.innerHTML);
-});
-console.log(arr);
-
-for (let i = 0; i < 8; i++) {
-  let milkType = document.querySelector(
-    `#milkTable > table > tbody > tr:nth-child(${
-      i + 1
-    }) > th:nth-child(3) > input`
+const getMilkRate = async () => {
+  const response = await fetch(
+    "https://milk-rate-default-rtdb.asia-southeast1.firebasedatabase.app/milk.json"
   );
-  milkType.addEventListener("input", (e) => {
-    let value = parseFloat(milkType.value);
-    console.log(value);
 
-    let milk_costs = document.querySelector(
-      `#milkTable > table > tbody > tr:nth-child(${i + 1}) > th:nth-child(4)`
+  if (!response.ok) {
+    throw new Error("Something Went Wrong!!!");
+  }
+  const responseData = await response.json();
+
+  let loadedMilkRate = [];
+  for (const key in responseData) {
+    loadedMilkRate.push({
+      id: key,
+      name: responseData[key].name,
+      price: responseData[key].price,
+    });
+  }
+  return loadedMilkRate;
+};
+
+getMilkRate().then((milk) => {
+  console.log(milk);
+  let arr = [];
+  let body = document.querySelector(`tbody`);
+  let t = "";
+  milk.forEach((val) => {
+    t += `<tr>
+      <th>${val.name}</th>
+      <th>${val.price}</th>
+      <th>
+        <input
+          class="column is-4 is-small input is-rounded is-normal is-info"
+          type="text"
+          placeholder="0"
+          oninput="this.value=this.value.replace(/[^0-9]/g,'')"
+        />
+      </th>
+      <th class="i_costs"></th>
+    </tr>`;
+    arr.push(+val.price);
+  });
+  console.log(arr);
+  body.innerHTML = t;
+  for (let i = 0; i < 8; i++) {
+    let milkType = document.querySelector(
+      `#milkTable > table > tbody > tr:nth-child(${
+        i + 1
+      }) > th:nth-child(3) > input`
     );
-    if (!isNaN(value)) {
-      milk_costs.innerHTML = value * arr[i];
-    } else {
-      milk_costs.innerHTML = 0;
-    }
+    milkType.addEventListener("input", (e) => {
+      let value = parseFloat(milkType.value);
+      console.log(value);
 
-    let total = 0;
-
-    for (let i = 0; i < 8; i++) {
-      let milk = document.querySelector(
+      let milk_costs = document.querySelector(
         `#milkTable > table > tbody > tr:nth-child(${i + 1}) > th:nth-child(4)`
       );
-      console.log(milk.innerHTML);
-      if (milk.innerHTML != "") {
-        let t_value = parseFloat(milk.innerHTML);
-        total += t_value;
+      if (!isNaN(value)) {
+        milk_costs.innerHTML = value * arr[i];
+      } else {
+        milk_costs.innerHTML = 0;
       }
-    }
 
-    total_value = document.querySelector(
-      "#milkTable > table > tfoot > tr > th.total_value"
-    );
-    total_value.innerHTML = total;
-  });
-}
+      let total = 0;
+
+      for (let i = 0; i < 8; i++) {
+        let milk = document.querySelector(
+          `#milkTable > table > tbody > tr:nth-child(${
+            i + 1
+          }) > th:nth-child(4)`
+        );
+        console.log(milk.innerHTML);
+        if (milk.innerHTML != "") {
+          let t_value = parseFloat(milk.innerHTML);
+          total += t_value;
+        }
+      }
+
+      total_value = document.querySelector(
+        "#milkTable > table > tfoot > tr > th.total_value"
+      );
+      total_value.innerHTML = total;
+    });
+  }
+});
